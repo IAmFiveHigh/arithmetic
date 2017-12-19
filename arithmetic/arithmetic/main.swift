a8458641f6910bab315e117027b741a2b40bb92a
IAmFiveHigh <745170149@qq.com>
2017-12-19 11:12:01 +0800

    添加插入排序

--- arithmetic/arithmetic/main.swift
+++ arithmetic/arithmetic/main.swift
@@ -50,12 +50,34 @@ func selectSort(arr: [Int]) -> [Int] {
     return array
 }
 
+//插入排序
+func insertSort(arr: [Int]) -> [Int] {
+    
+    var array = [Int]()
+    let n = arr.count
+    
+    for i in 0..<n {
+        var j = i
+        let b = arr[i]
+        
+        array.append(arr[i])
+        while j>0 && array[j-1] > b {
+            swap(&array[j], &array[j-1])
+            j -= 1
+        }
+    }
+    
+    return array
+}
 
+//归并排序
 
 var arr1 = SortTextHelper.generateRandomArray(n: 10000, rangL: 0, rangR: 10000)
 if let arr1 = arr1 {
     SortTextHelper.sortTimeTest(title: "冒泡排序", array: arr1, function: bubbleSort)
     SortTextHelper.sortTimeTest(title: "选择排序", array: arr1, function: selectSort)
+    SortTextHelper.sortTimeTest(title: "插入排序", array: arr1, function: insertSort)
+
 }
 
 
